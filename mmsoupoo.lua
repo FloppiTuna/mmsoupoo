065924b19655e17734379a79412ffbdeebffe1e3
Griffin <griffin@edforma.app>
2025-10-16 18:15:52 -0500

    uh

--- mmsoupoo.lua
+++ mmsoupoo.lua
@@ -1,12 +1,10 @@
-local aukit = require "aukit"
-
 print(require("./data/splash"))
 
 -- Load the peripheral map (periph.map).
 local periphMap
 
-if fs.exists("./periph.map") then
-    local mapFile = fs.open("./periph.map", "r")
+if fs.exists("./data/periph.map") then
+    local mapFile = fs.open("./data/periph.map", "r")
     local mapContent = mapFile.readAll()
 
     -- Parse the file as JSON.
@@ -68,9 +66,9 @@ end
 -- Load and run modules in parallel
 local modules = {
     -- "modules/franky",
-    "modules/ambience",
-    "modules/remote",
-    "modules/overwatch"
+    "./modules/ambience",
+    "./modules/remote",
+    "./modules/overwatch"
 }
 
 local moduleRunners = {}
