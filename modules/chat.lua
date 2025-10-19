6eca44fa18a65b72d0ffd5ee74889fbbb7b588a8
Cat6a <130535199+Cat6a-debug@users.noreply.github.com>
2025-10-19 18:12:48 -0500

    beginning of a long journey

--- modules/chat.lua
+++ modules/chat.lua
@@ -8,10 +8,11 @@ function init(common)
         local event, username, message, uuid, isHidden, messageUtf8 = os.pullEvent("chat")
         local wakeword = string.find(message, "mmsoup")
         if wakeword == 1 then
+            logger.log("WakeWord detected, parsing message: ".. message)
+
             local words = {}
             for str in string.gmatch(message, "([^".." ".."]+)") do
                 table.insert(words,str)
-                logger.log(str)
             end
         end
     end
